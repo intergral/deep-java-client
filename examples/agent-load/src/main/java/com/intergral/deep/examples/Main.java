35b44c46c2a7a5e5fd81d9b665faf21d7e8867dd
Ben Donnelly <ben_donnelly@intergral.com>
2023-05-31 18:38:08 +0100

    chore(docs): update names and descriptions of modules

--- examples/agent-load/src/main/java/com/intergral/deep/examples/Main.java
+++ examples/agent-load/src/main/java/com/intergral/deep/examples/Main.java
@@ -17,55 +17,10 @@
 package com.intergral.deep.examples;
 
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-
 public class Main
 {
     public static void main( String[] args ) throws Throwable
     {
-        new Main().name();
-        Path jarPath = Paths.get( Main.class.getResource( "/" ).toURI() )
-                .getParent()
-                .getParent()
-                .getParent()
-                .getParent()
-                .resolve( "agent/target/nerdvision-1.0-SNAPSHOT.jar" );
-        System.setProperty( "nv.jar.path", jarPath.toString() );
-//        try
-//        {
-//            System.out.println( NerdVisionAPI.getInstance().version().version() );
-//        }
-//        catch( Exception e )
-//        {
-//            e.printStackTrace();
-//        }
-//        final NerdVision nv = NerdVision.getInstance();
-//        nv.await();
-        //        nv.configure( new HashMap<String, String>()
-        //        {{
-        //            put( "logging.level", "DEBUG" );
-        //        }} );
-//        NerdVision.start(
-//                "nv-2AWmgeFph8M8ZI0HUW5K",
-//                "java-poc", new HashMap<String, String>()
-//                {{
-//                    put( "key", "val" );
-//                }} );
-//        System.out.println( NerdVisionAPI.getInstance().version().version() );
-//        //        nv.addBreakpoint( Main.class, 37 );
-//        NerdVisionAPI.getInstance().updateBreakpoints();
-
-//        int cnt = 0;
-//        while( cnt < 1000 )
-//        {
-//            new Main().something( "hello me" );
-//            Thread.sleep( 1000 );
-//            cnt++;
-//        }
-
         final SimpleTest ts = new SimpleTest( "This is a test", 2 );
         for( ; ; )
         {
@@ -76,76 +31,9 @@ public class Main
             catch( Exception e )
             {
                 e.printStackTrace();
-//                NerdVisionAPI.getInstance().captureException( e );
             }
 
             Thread.sleep( 1000 );
         }
-
-    }
-
-    public void something( final String name )
-    {
-        System.out.println( "something" );
-        System.out.println( name );
-        System.out.println( "something" );
-    }
-
-    public static class Person
-    {
-        private final Person parent;
-        private final String name;
-
-
-        public Person( final String name )
-        {
-            this( null, name );
-        }
-
-
-        public Person( final Person parent, final String name )
-        {
-            this.parent = parent;
-            this.name = name;
-        }
-
-
-        public Person getParent()
-        {
-            return parent;
-        }
-
-
-        public String getName()
-        {
-            return name;
-        }
-    }
-
-
-    public void name() throws Exception
-    {
-        final HashMap<String, String> hashMap = new HashMap<String, String>()
-        {{
-            put( "name", "ben" );
-        }};
-        javax.script.ScriptEngineManager mgr = new javax.script.ScriptEngineManager();
-        final List<javax.script.ScriptEngineFactory> engineFactories = mgr.getEngineFactories();
-
-        for( javax.script.ScriptEngineFactory engineFactory : engineFactories )
-        {
-            System.out.println( engineFactory.getNames() );
-        }
-
-        javax.script.ScriptEngine engine = mgr.getEngineByName( "JavaScript" );
-        final javax.script.Bindings bindings = engine.createBindings();
-        bindings.put( "obj", hashMap );
-        bindings.put( "person", new Person( new Person( "mary" ), "bob" ) );
-
-        System.out.println( engine.eval( "person", bindings ) );
-        System.out.println( engine.eval( "person.getParent()", bindings ) );
-        System.out.println( engine.eval( "System.exit()", bindings ) );
-        System.out.println( engine.eval( "person.name", bindings ) );
-        System.out.println( engine.eval( "person.parent.name == 'mary'", bindings ) );
     }
 }
--- examples/dynamic-load/src/main/java/com/intergral/deep/examples/Main.java
+++ examples/dynamic-load/src/main/java/com/intergral/deep/examples/Main.java
@@ -24,14 +24,13 @@ import com.intergral.deep.agent.api.reflection.IReflection;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
 
 public class Main
 {
     public static void main( String[] args ) throws Throwable
     {
-        new Main().name();
+        // this is only needed in this example as we are using a local built module
+        // if using the dependency from maven you do not need to set the path
         Path jarPath = Paths.get( Main.class.getResource( "/" ).toURI() )
                 .getParent()
                 .getParent()
@@ -39,8 +38,8 @@ public class Main
                 .getParent()
                 .resolve( "agent/target/deep-1.0-SNAPSHOT.jar" );
         System.setProperty( "deep.jar.path", jarPath.toString() );
+
         Deep.config()
-                .setValue( "logging.level", "FINE" )
                 .setValue( "service.url", "localhost:43315" )
                 .setValue( "service.secure", false )
                 .start();
@@ -48,38 +47,6 @@ public class Main
         final Deep instance = Deep.getInstance();
         System.out.println( instance.<IDeep>api().getVersion() );
         System.out.println( instance.<IReflection>reflection() );
-
-//        try
-//        {
-//            System.out.println( NerdVisionAPI.getInstance().version().version() );
-//        }
-//        catch( Exception e )
-//        {
-//            e.printStackTrace();
-//        }
-//        final NerdVision nv = NerdVision.getInstance();
-//        nv.await();
-        //        nv.configure( new HashMap<String, String>()
-        //        {{
-        //            put( "logging.level", "DEBUG" );
-        //        }} );
-//        NerdVision.start(
-//                "nv-2AWmgeFph8M8ZI0HUW5K",
-//                "java-poc", new HashMap<String, String>()
-//                {{
-//                    put( "key", "val" );
-//                }} );
-//        System.out.println( NerdVisionAPI.getInstance().version().version() );
-//        //        nv.addBreakpoint( Main.class, 37 );
-//        NerdVisionAPI.getInstance().updateBreakpoints();
-
-//        int cnt = 0;
-//        while( cnt < 1000 )
-//        {
-//            new Main().something( "hello me" );
-//            Thread.sleep( 1000 );
-//            cnt++;
-//        }
 
         final SimpleTest ts = new SimpleTest( "This is a test", 2 );
         for( ; ; )
@@ -91,72 +58,10 @@ public class Main
             catch( Exception e )
             {
                 e.printStackTrace();
-//                NerdVisionAPI.getInstance().captureException( e );
             }
 
             Thread.sleep( 1000 );
         }
 
-    }
-
-    public void something( final String name )
-    {
-        System.out.println( "something" );
-        System.out.println( name );
-        System.out.println( "something" );
-    }
-
-    public void name() throws Exception
-    {
-        final HashMap<String, String> hashMap = new HashMap<String, String>()
-        {{
-            put( "name", "ben" );
-        }};
-        javax.script.ScriptEngineManager mgr = new javax.script.ScriptEngineManager();
-        final List<javax.script.ScriptEngineFactory> engineFactories = mgr.getEngineFactories();
-
-        for( javax.script.ScriptEngineFactory engineFactory : engineFactories )
-        {
-            System.out.println( engineFactory.getNames() );
-        }
-
-        javax.script.ScriptEngine engine = mgr.getEngineByName( "JavaScript" );
-        final javax.script.Bindings bindings = engine.createBindings();
-        bindings.put( "obj", hashMap );
-        bindings.put( "person", new Person( new Person( "mary" ), "bob" ) );
-
-        System.out.println( engine.eval( "person", bindings ) );
-        System.out.println( engine.eval( "person.getParent()", bindings ) );
-    }
-
-    public static class Person
-    {
-        private final Person parent;
-        private final String name;
-
-
-        public Person( final String name )
-        {
-            this( null, name );
-        }
-
-
-        public Person( final Person parent, final String name )
-        {
-            this.parent = parent;
-            this.name = name;
-        }
-
-
-        public Person getParent()
-        {
-            return parent;
-        }
-
-
-        public String getName()
-        {
-            return name;
-        }
     }
 }
